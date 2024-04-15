34a3d7a70345b3dd7343701cb9ed2a9f78f875b0
unknown <ea.avdeev@gmail.com>
2024-04-15 12:21:29 +0300

    build(app): redux-toolkit,redux-devtools-extension

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import {useDispatch, useSelector} from "react-redux";
 
 function App() {
+  const dispatch = useDispatch()
+  const cash = useSelector(state => state.cashReducer.cash) // 'cash' - это cashReducer, затем переменная 'cash' получаемая из состояния
+    console.log(cash)
+
+// cash. 
+    const addCash = (cash) => { // в dispatch прокидываем action {type='',paylod='?'}, и state изменяется 
+      if (!isNaN(cash) && cash !== 0) {
+        dispatch({type: 'ADD_CASH', payload: cash})      
+      }
+    }
+
+    const getCash = (cash) => {
+      dispatch({type: 'GET_CASH', payload: cash})
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <p>My Bank</p>
+        </header>
+
+{/* view */}
+          {/* cash */}
+          <div style={{fontSize: '2rem', marginTop: '100px'}}>
+              {cash}
+          </div>
+          <div style={{display: 'flex', justifyContent: 'center', marginTop: '30px'}}>
+              <button onClick={() => addCash(Number(prompt()))}>Добавить средства</button>
+              <button onClick={() => getCash(Number(prompt()))}>Снять средства</button>
+          </div>
+
+      </div>
   );
 }
 
